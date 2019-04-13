29c2951abc34e8f267a2d0be26f409cca81a1210
G-Chilie <okonkwolinda7@gmail.com>
2019-04-13 23:23:00 +0100

    feature(activate or deactivate):add activate or deactivate account endpoint[Finishes #165028915]

--- app/middleware/formValidator.js
+++ app/middleware/formValidator.js
@@ -75,13 +75,14 @@ export default class Validator {
             .json({ message: 'email cannot be empty' });
         }
         next();
-        return true;
+        // return true;
       } catch (error) {
         return res.status(400).json({
           status: 400,
           error: 'JSON object should contain {  email, password }',
         });
       }
+      return true;
     }
   }
   
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,8 @@ import UserController from '../controllers/userController';
 import Validator from '../middleware/formValidator';
 import AccountValidator from '../middleware/accountValidator';
 import Verifyuser from '../helpers/auth';
+import StaffController from '../controllers/staffController';
+
 
 
 const router = express.Router();
@@ -13,6 +15,8 @@ const { createUser, userLogin } = UserController;
 const { createAccount } = AccountController;
 const { userValidation, loginValidation } = Validator;
 const { acctValidation } = AccountValidator;
+const { ActivatOrDeactivateAccct, deleteAccount, creditAccount, debitAccount
+  } = StaffController;
 
 
 
@@ -21,5 +25,7 @@ router.post('/auth/signup', userValidation, createUser);
 router.post('/auth/login', loginValidation, userLogin);
 router.post('/accounts', acctValidation, Verifyuser, createAccount);
 
+// admin routes
+router.patch('/accounts/:accountNumber', Verifyuser, ActivatOrDeactivateAccct);
 
 export default router;
