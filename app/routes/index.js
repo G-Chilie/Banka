69ad09ae6f2d1bacfd1e54175705ca26ce83baf1
G-Chilie <okonkwolinda7@gmail.com>
2019-04-22 23:57:35 +0100

    feature(login):add login endpoint [Finishes #165482849]

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,40 @@
 import express from 'express';
 
-// import AccountController from '../controllers/accountController';
-// import StaffController from '../controllers/staffController';
+import AccountController from '../controllers/accountController';
+import StaffController from '../controllers/staffController';
 import UserController from '../controllers/userController';
 import { schema, validate } from '../middleware/schemaValidators';
-// import Auth from '../middleware/auth';
-// import urlMiddleware from '../middleware/url';
+import Auth from '../middleware/auth';
+import urlMiddleware from '../middleware/url';
 
 
 const router = express.Router();
 
-const {createUser} = UserController;
-
-
+const { createUser, loginUser } = UserController;
+const { createAccount, getAllTransactions, getTransaction } = AccountController;
+const {
+  ActivatOrDeactivateAccct, getAllAccounts, deleteAccount, creditAccount,
+  debitAccount, getUserAccounts,
+} = StaffController;
+const { verifyToken } = Auth;
+const { verifyAccountNumber } = urlMiddleware;
 
 // client routes
 router.post('/auth/signup', validate(schema.userSchema), createUser);
+router.post('/auth/login', validate(schema.loginSchema), loginUser);
+router.post('/accounts', validate(schema.accountsSchema), verifyToken, createAccount);
+router.get('/accounts/:accountNumber/transactions', verifyAccountNumber, verifyToken, getAllTransactions);
+router.get('/transactions/:transactionId', verifyToken, getTransaction);
+
+// cashier routes
+router.post('/transactions/:accountNumber/credit', validate(schema.transactionsSchema), verifyAccountNumber, verifyToken, creditAccount);
+router.post('/transactions/:accountNumber/debit', validate(schema.transactionsSchema), verifyAccountNumber, verifyToken, debitAccount);
+
+// admin routes
+router.patch('/accounts/:accountNumber', validate(schema.activeDeactivateSchema), verifyAccountNumber, verifyToken, ActivatOrDeactivateAccct);
+router.delete('/accounts/:accountNumber', verifyAccountNumber, verifyToken, deleteAccount);
+router.get('/accounts', verifyToken, getAllAccounts);
+router.get('/user/:userEmail/accounts', verifyToken, getUserAccounts);
+
 
 export default router;
