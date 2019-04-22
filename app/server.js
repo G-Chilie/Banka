49cd50b2f637be23199a67d6bd86e65a2205d5a3
G-Chilie <okonkwolinda7@gmail.com>
2019-04-22 23:00:02 +0100

    feature(signup):add signup endpoint [Fininshes #165482848]

--- app/server.js
+++ app/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import router from './routes';
 
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
